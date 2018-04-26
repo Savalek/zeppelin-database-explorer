fbcb1dec8c93ca93351de511724a7811c035f9f7
Savalek <def113@mail.ru>
2018-04-26 15:15:46 +0300

    Hide Explorer on start

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+// ---------------------   jsTree - license    ------------------------
+//                                                 -------- MIT license
+// https://www.jstree.com/
+// Copyright (c) 2014 Ivan Bozhanov
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+//     copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+//     The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 import {
     SpellBase,
     SpellResult,
@@ -62,7 +89,7 @@ function createOnDestroyListener() {
         } catch (e) {
         }
         if (notebookController && contentDOM) {
-            showDBExplorer();
+            //showDBExplorer();
             this.disconnect();
         }
 
@@ -239,7 +266,6 @@ function SearchEngine(jstree) {
     this.MAX_NODES_TO_SKIP = 20;
     let MAX_NODES_TO_SHOW = 300;
     this.skip_count = 0;
-    let timerId;
     let countShownNode = this.MAX_NODES_TO_SKIP;
 
     this.runSearch = function run() {
@@ -258,7 +284,7 @@ function SearchEngine(jstree) {
         }
 
         if (countShownNode < MAX_NODES_TO_SHOW) {
-            timerId = setTimeout(run, 100);
+            setTimeout(run, 100);
         } else {
             jstreeSearchEngine = null;
             showMaxSearchElementBlock(true);
@@ -604,11 +630,3 @@ function injectCSS() {
 </style>
 `).appendTo('head')
 }
-
-
-
-
-
-
-
-
